5ba7858e99a7e92108c9ae992756552ab7a54c9d
yunhoo0 <86551168+Rei-L0@users.noreply.github.com>
2022-06-08 16:45:11 +0900

    9095(1,2,3 더하기)
    Clear

---
+++ 9095-1,2,3 더하기.py
@@ -0,0 +1,20 @@
+import re
+
+
+T = int(input())
+
+
+def sol(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+    else:
+        return sol(n-1)+sol(n-2)+sol(n-3)
+
+
+for _ in range(T):
+    N = int(input())
+    print(sol(N))
